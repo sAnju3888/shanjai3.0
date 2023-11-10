c1fdf0558c8658475a88088bf85914ae75a03c9f
sAnju3888 <114935971+sAnju3888@users.noreply.github.com>
2023-11-10 10:25:42 +0530

    updated version

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3030;
+app.listen(PORT);
+app.use(express.static("public"))
+app.set('view engine','ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+
+app.get('/home',(req,res)=>{
+    res.render('home');
+});
+
+
+app.get('/project',(req,res)=>{
+    res.render('project');
+});
+
+app.get('/bio',(req,res)=>{
+    res.render('bio');
+});
